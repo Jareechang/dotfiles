a7576e3e24b36ea455174cae8f3ce6cbf6fa08e5
Jerry Chang <therealjerrycan@gmail.com>
2016-09-12 20:16:53 -0700

    added more bundles to install shell script

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
- git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
- vi +PluginInstall +qall
+# Install vundle bunlders 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install auto close 
+git clone git://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
+vi +PluginInstall +qall
